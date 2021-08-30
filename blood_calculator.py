442e001dc0f41aee2bd0b10a4476861142cec03d
Kerry Eller <kerryeller06@gmail.com>
2021-08-30 15:15:32 -0400

    Added functions that allow characterization of LDL levels.

--- blood_calculator.py
+++ blood_calculator.py
@@ -4,13 +4,16 @@ def interface():
     while keep_running:
         print("Make a choice")
         print("9 = Quit")
+        print("1 = HDL Analysis")
+        print("2 = LDL Analysis")
         choice = int(input("Make a choice: "))
         #The input would be a string, so converting it to an integer to do comparisons
         if choice == 9:
             keep_running = False 
-        else :
+        elif choice == 1 :
             HDL_Driver()
-        
+        elif choice == 2 :
+            LDL_Driver()
     
     print(choice)
     return choice
@@ -27,17 +30,42 @@ def hdl_input():
 	
 def hdl_analysis(HDL_Value): 
     if HDL_Value >= 60:
-        hdl_char = "Normal"
+        hdl_char = "normal"
     elif ((HDL_Value < 60) & (HDL_Value >= 40)):
-        hdl_char = "Borderline Low"
+        hdl_char = "borderline low"
     elif HDL_Value < 40:
-        hdl_char = "Low"     
+        hdl_char = "low"     
         
     return hdl_char
     
 def hdl_output(HDL_Value,HDL_Character): 
-    print("At an HDL Value of ", HDL_Value, ", your cholesterol is ",HDL_Character)) 
+    print("At an HDL Value of ",HDL_Value,", your cholesterol is ",HDL_Character) 
+
+def LDL_Driver(): 
+    LDL_Value = ldl_input()
+    LDL_Character = ldl_analysis(LDL_Value)
+    ldl_output(LDL_Value, LDL_Character) 
+    
+def ldl_input(): 
+    ldl_value = int(input(("Enter LDL Value: ")))
+    return ldl_value
+	
+def ldl_analysis(LDL_Value): 
+    if LDL_Value >= 190:
+        ldl_char = "very high"
+    elif ((LDL_Value > 160) & (LDL_Value <= 189)):
+        ldl_char = "high"
+    elif ((LDL_Value >= 130) & (LDL_Value <= 159)):
+        ldl_char = "borderline high"
+    elif LDL_Value < 130:
+        ldl_char = "normal"     
+        
+    return ldl_char
+    
+def ldl_output(LDL_Value,LDL_Character): 
+    print("At an LDL Value of ",LDL_Value, ", your cholesterol is ",LDL_Character)
    
     
 HDL_Driver() 
+LDL_Driver()
 interface() 
